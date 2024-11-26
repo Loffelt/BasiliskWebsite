e6d06258e55b8ea0bc853242de51fbe1c6d99cf3
Isaac_Lagoy <isaacblagoy@gmail.com>
2024-11-26 13:40:43 -0600

    Added carbon design

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,24 @@
+<script lang='ts'>
+    import {
+        Header,
+        HeaderUtilities,
+        HeaderGlobalAction,
+        SideNav,
+        SideNavItems,
+        SideNavLink,
+        Content,
+        Grid
+    } from 'carbon-components-svelte';
+</script>
+
+<Header
+    platformName='Basilisk Engine'
+>
+
+</Header>
+
+<Content>
+    <Grid>
+        <slot/>
+    </Grid>
+</Content>
--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -1,1 +1,3 @@
+import "carbon-components-svelte/css/g90.css";
+import '$lib/app.css'
 export const prerender = true;
